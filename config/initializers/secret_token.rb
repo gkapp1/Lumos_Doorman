1d46f509a65e018b60f44dac61918f9cd945e9c7
Greg Kapp <gregkapp@gmail.com>
2013-07-28 16:56:12 -0700

    Initialize repository for Lumos Digital Doorman Part Deux

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LumosDoorman::Application.config.secret_key_base = '3f0113044982506af8596c50f93809765abd01830f8908c8472bc4c2d78b7fbb1c93791ba05d1313154a800c7ecbf57e21f9a626c0452f6254bd9c35c62ac004'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LumosDoorman::Application.config.session_store :cookie_store, key: '_Lumos_Doorman_session'
